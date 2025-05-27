92eded1dd6b8ad2e63099126966be833cc588413
aldimf26 <aldimf26@gmail.com>
2025-05-27 22:43:10 +0800

    dsa

--- resources/views/components/maz-sidebar.blade.php
+++ resources/views/components/maz-sidebar.blade.php
@@ -21,7 +21,7 @@
             </div>
             
             <div class="d-flex justify-content-center align-items-center mt-4">
-                <div class="theme-toggle d-flex gap-2 align-items-center">
+                <div class="theme-toggle d-flex gap-2 align-items-center d-none">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                         aria-hidden="true" role="img" class="iconify iconify--system-uicons" width="20"
                         height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 21 21">
@@ -37,10 +37,10 @@
                             </g>
                         </g>
                     </svg>
-                    {{-- <div class="form-check form-switch fs-6">
+                    <div class="form-check form-switch fs-6">
                         <input class="form-check-input me-0" type="checkbox" id="toggle-dark" style="cursor: pointer">
                         <label class="form-check-label"></label>
-                    </div> --}}
+                    </div>
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                         aria-hidden="true" role="img" class="iconify iconify--mdi" width="20" height="20"
                         preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
